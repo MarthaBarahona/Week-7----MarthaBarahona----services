07ba9b640b6c53cd20d3bdc24b2640274de064ce
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-20 10:53:39 -0600

    Added components to the model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
