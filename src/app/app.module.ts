06ccfc8e17a29c353462d001d7f6b84b7f61084d
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-20 15:42:43 -0600

    Added reactiveformmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './components/container/container.component';
 import { SearchComponent } from './components/search/search.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
-    SearchComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
