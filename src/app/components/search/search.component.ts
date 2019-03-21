729b62f3e5382d74079c5d8ca4d0b30d0e281535
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-21 10:22:39 -0600

    Deleted spaces

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -2,7 +2,7 @@ import { UserData } from './interfaces/userData.interface';
 import { User } from './interfaces/user.interface';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { DataService } from './../../services/data.service';
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { forkJoin } from 'rxjs';
 
 @Component({
@@ -10,22 +10,18 @@ import { forkJoin } from 'rxjs';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.sass']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
   form: FormGroup;
   showed = false;
   hasData = false;
   user: User | any;
   userData: UserData | any;
 
-  @Output()name = new EventEmitter();
-
   constructor(private service: DataService, private fb: FormBuilder) {
     this.form = fb.group({
       username: []
     });
   }
-
-  ngOnInit() {}
 
   get username() {
     return this.form.get('username');
@@ -38,27 +34,32 @@ export class SearchComponent implements OnInit {
         this.user = response;
         const resu = forkJoin(
           this.service.getUserData(this.user.items[0].login),
+          this.service.getUserFollowers(this.user.items[0].followers_url),
           this.service.getUserRepos(this.user.items[0].login)
         ).subscribe((userDataResponse: User|any) => {
           this.showed = false;
           this.hasData = true;
-          console.log(userDataResponse);
-          this.joinInfo(userDataResponse[0], userDataResponse[1]);
+          this.joinInfo(userDataResponse[0], userDataResponse[1], userDataResponse[2]);
           });
         });
   }
 
-  joinInfo(data, repositories) {
+  joinInfo(data, followersList, repositories) {
     this.userData = {
       avatar_url : data.avatar_url,
       name: data.name,
       following: data.following,
       company: data.company,
       blog: data.blog,
-      location: data.string,
+      location: data.location,
       created_at: data.created_at,
-      html_url: data.html_url
+      html_url: data.html_url,
+      repositories: repositories.items,
+      number_repositories: repositories.total_count,
+      followers: followersList
     };
     console.log(this.userData);
+    console.log(followersList);
+    console.log(repositories.items);
   }
 }
