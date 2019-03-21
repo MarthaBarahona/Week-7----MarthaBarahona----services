8f380776c26a66e983f2ab34688433988742a034
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-21 08:05:09 -0600

    Added folder with interfaces

---
+++ src/app/components/search/interfaces/user.interface.ts
@@ -0,0 +1,4 @@
+export interface User {
+  items: [{}];
+  login: string;
+}
---
+++ src/app/components/search/interfaces/userData.interface.ts
@@ -0,0 +1,10 @@
+export interface UserData {
+    avatar_url: string;
+    name: string;
+    following: number;
+    company: string | null;
+    blog: string | null;
+    location: string;
+    created_at: string;
+    html_url: string;
+}
