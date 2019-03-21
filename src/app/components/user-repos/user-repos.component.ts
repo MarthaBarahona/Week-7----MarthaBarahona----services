603bce85290f197cb339bb89848b6f08055cc509
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-21 10:17:19 -0600

    Created user-repos component

---
+++ src/app/components/user-repos/user-repos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-repos',
+  templateUrl: './user-repos.component.html',
+  styleUrls: ['./user-repos.component.sass']
+})
+export class UserReposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
