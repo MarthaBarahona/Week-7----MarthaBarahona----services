0f6d1c4466b3f2d7de5ed6103c61660783f0bdaf
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-21 10:18:09 -0600

    Added user-repos component to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { DisplayDataComponent } from './components/display-data/display-data.component';
+import { UserReposComponent } from './components/user-repos/user-repos.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    DisplayDataComponent
+    DisplayDataComponent,
+    UserReposComponent
   ],
   imports: [
     BrowserModule,
