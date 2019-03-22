bdb2d78532c59fd77c9a27410b97379e6fe07c02
MarthaBarahona <martha.barahona.10@gmail.com>
2019-03-21 23:21:06 -0600

    Changed style sheets extentions

--- src/app/components/user-followers/user-followers.component.ts
+++ src/app/components/user-followers/user-followers.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-followers',
   templateUrl: './user-followers.component.html',
-  styleUrls: ['./user-followers.component.sass']
+  styleUrls: ['./user-followers.component.scss']
 })
 export class UserFollowersComponent implements OnInit {
-
+  @Input() userFollowers: [{}];
   constructor() { }
 
   ngOnInit() {
--- src/app/components/user-repos/user-repos.component.ts
+++ src/app/components/user-repos/user-repos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-user-repos',
   templateUrl: './user-repos.component.html',
-  styleUrls: ['./user-repos.component.sass']
+  styleUrls: ['./user-repos.component.scss']
 })
 export class UserReposComponent implements OnInit {
   @Input() userRepos: [{}];
